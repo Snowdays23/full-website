abea24665ec875fca06de7b92ee3b2d5d6e484f0
Andrea Esposito <aespositox@gmail.com>
2022-12-10 15:05:11 +0100

    Quick fix for inlinecss

--- snowdays23/settings.py
+++ snowdays23/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import dj_database_url
 import os
 
+# Quick and dirty fix for outdated library (inlinecss)
+from django.utils.encoding import smart_str
+django.utils.encoding.smart_text = smart_str
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
