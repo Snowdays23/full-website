31d174436ed1beb385bcb1caee3650e371c5cce9
Andrea Esposito <aespositox@gmail.com>
2023-02-17 00:05:53 +0100

    Fixed event retrieve by slug view

--- sd23log/views.py
+++ sd23log/views.py
@@ -43,7 +43,7 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, slug=None):
         event = get_object_or_404(self.get_queryset(), slug=slug)
-        serializer = self.get_serializer_class()
+        serializer = self.get_serializer_class()(event)
         return Response(serializer.data)
 
     def list(self, request, **kwargs):
