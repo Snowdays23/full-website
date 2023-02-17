68cbd38ce271e1a445045cf4ca3bf5ee32feb446
Andrea Esposito <aespositox@gmail.com>
2023-02-17 01:21:00 +0100

    Added missing import

--- sd23log/views.py
+++ sd23log/views.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+
 from django.conf import settings
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
